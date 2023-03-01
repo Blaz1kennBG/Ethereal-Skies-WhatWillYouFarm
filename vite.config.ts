3bfa4e47b1086f700a2bf3a1ae8aea2901ce5ab1
Blaz1kennbg <73253075+Blaz1kennBG@users.noreply.github.com>
2023-03-02 01:36:17 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -31,5 +31,6 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  base: process.env.NODE_ENV === "production" ? "/dist/" : "/",
+  base:
+    process.env.NODE_ENV === "production" ? "/ethereal-skies-wwyf/dist" : "/",
 });
